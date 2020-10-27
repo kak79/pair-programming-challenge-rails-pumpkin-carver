e85ffd84b0bfb7aab21f6a5d3964bc7888758bc0
brewchetta <chett.tiller@gmail.com>
2020-10-27 09:08:04 -0400

    Built farms controller

---
+++ app/controllers/farms_controller.rb
@@ -0,0 +1,36 @@
+class FarmsController < ApplicationController
+
+  def index
+    @farms = Farm.all
+  end
+
+  def show
+    @farm = Farm.find_by_id(params[:id])
+    if !@farm
+      flash[:errors] = ["Could not find a farm with the id #{params[:id]}"]
+      redirect_to farms_path
+    end
+  end
+
+  def new
+    @farm = Farm.new
+  end
+
+  def create
+    @farm = Farm.new(farm_params)
+    if @farm.save
+      flash[:messages] = ["#{@farm.name} successfully created"]
+      redirect_to farm_path(@farm)
+    else
+      flash[:errors] = @farm.errors.full_messages
+      redirect_to new_farm_path
+    end
+  end
+
+  private
+
+  def farm_params
+    params.require(:farm).permit(:name, :img_url, :zipcode, :description)
+  end
+
+end
